2f7e7b833fdaed92377559494aef9e11f1b052d4
dabere <daberechukwu18@gmail.com>
2024-10-19 14:00:01 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict";
+// // const dosomething = ()=>{
+// //     console.log("name")
+// //     return "name";
+// // }
+let User = {
+    name: "dabere",
+    age: 12,
+    isStudent: true,
+};
---
+++ main.ts
@@ -0,0 +1,54 @@
+// // const dosomething = ()=>{
+// //     console.log("name")
+// //     return "name";
+// // }
+
+// // let myName:string = "dabere"
+
+// // let numberofWheels:number  =  4;
+// // let condition:boolean = true;
+// // type student = {
+// //     name:string,
+// //     age: number,
+// //     gender:string,
+// //     active:boolean,
+// // }
+
+// // let student:student = {
+// //     name: "dabere",
+// //     age: 15,
+// //     gender: "male",
+// //     active:true,
+// // }
+// type menu = {
+//     name: string,
+//     price: number,
+// }
+
+// let menu:menu[] = [
+//     {
+//         name: "eggPizza",
+//         price: 10,
+        
+//     },
+//     {
+//        name: "ricePizza",
+//        price: 15,
+//     },
+//     {
+//         name:"beansPizza",
+//         price: 8.00
+//     }
+// ]
+
+interface user {
+    name:string,
+    age:number,
+    isStudent:boolean,
+}
+
+let User:user = {
+    name:"dabere",
+    age:12,
+    isStudent:true,
+}
